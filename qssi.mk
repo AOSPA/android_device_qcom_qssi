2e9e4c81015ab3ffc7d331da17b8cfb28ecc5a85
Alexander Koskovich <zvnexus@outlook.com>
2020-10-07 16:01:41 -0700

    qssi: Remove encryption bits.
     * Not all targets using this will use encryption.
    
    Change-Id: I076c338e72b23653c8b18efbed63b807b5f74cab

--- qssi.mk
+++ qssi.mk
@@ -247,10 +247,6 @@ TARGET_MOUNT_POINTS_SYMLINKS := false
 
 TARGET_USES_MKE2FS := true
 
-PRODUCT_PROPERTY_OVERRIDES += \
-ro.crypto.volume.filenames_mode = "aes-256-cts" \
-ro.crypto.allow_encrypt_override = true
-
 TARGET_USES_QCOM_DISPLAY_BSP := true
 
 ifeq ($(TARGET_USES_NEW_ION),true)
