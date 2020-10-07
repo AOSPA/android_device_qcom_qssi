f1889a746d4be00c5c8c2fc7ae1c06ea1f2916e0
Alexander Koskovich <zvnexus@outlook.com>
2020-10-07 16:19:00 -0700

    qssi: Remove TARGET_INIT_VENDOR_LIB.
    Change-Id: I2e5586f8f81263bbed6eb6e75123afaa4f296e1d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,7 +76,6 @@ BOARD_QTI_CAMERA_32BIT_ONLY := true
 TARGET_NO_RPC := true
 
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
-TARGET_INIT_VENDOR_LIB := libinit_msm
 
 #Disable appended dtb.
 TARGET_KERNEL_APPEND_DTB := false
