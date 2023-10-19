403113090ff86054a194e64d14cdfeaccca96d03
hoffc <quic_hoffc@quicinc.com>
2023-10-19 17:19:22 +0800

    qssi: disable initial status to stopped for some AOSP packages
    Disable the initial package status to stopped for some AOSP packages, or not, it will result in CTS test case failure after enabling new feature stopSystemPackagesByDefault.
    
    Change-Id: I7c1becc45c0975606840acabec31f4b4f5fdb9f3
    CRs-Fixed: 3603517

--- qssi.mk
+++ qssi.mk
@@ -296,6 +296,10 @@ endif
 # Enable support for APEX updates
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Enable allowlist for some aosp packages that should not be scanned in a "stopped" state
+# Some CTS test case failed after enabling feature config_stopSystemPackagesByDefault
+PRODUCT_PACKAGES += initial-package-stopped-states-aosp.xml
+
 ###################################################################################
 # This is the End of target.mk file.
 # Now, Pickup other split product.mk files:
